415e61b70f7421d19e311e956896f177408a4837
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2024-12-17 00:59:47 +0500

    Engine window running

---
+++ Game/src/main.cpp
@@ -0,0 +1,29 @@
+#include "Phezu.hpp"
+
+Phezu::Engine& engine = Phezu::CreateEngine();
+
+#ifdef __APPLE__
+
+int main(int argc, const char* argv[]) {
+    engine.Init("Tetris", 800, 600);
+    
+    //PrepareScenes(engine);
+    
+    engine.Run();
+}
+
+#elif _WIN32
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    engine.Init("Phezu", 800, 600);
+
+    //PrepareScenes(engine);
+
+    engine.Run();
+
+    return 0;
+}
+
+#endif
