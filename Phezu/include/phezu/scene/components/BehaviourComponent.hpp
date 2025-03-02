9666653892e77cee46a125235091d46240f5b08a
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2025-03-02 15:09:10 +0500

    Cleanup and pausing before redoing prefab system
    Now prefabs will be instantiated using their name

--- Phezu/include/phezu/scene/components/BehaviourComponent.hpp
+++ Phezu/include/phezu/scene/components/BehaviourComponent.hpp
@@ -4,8 +4,6 @@
 
 namespace Phezu {
     
-    class BehaviourComponentPrefabBase;
-    
     class BehaviourComponent : public DataComponent {
     public:
         BehaviourComponent(Entity* entity, uint8_t componentPrefabID) : DataComponent(entity), m_ComponentPrefabID(componentPrefabID) {}
