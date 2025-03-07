985462a2cd6be90426fd397c15097f93cef8ece9
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2025-03-07 21:14:05 +0500

    Removed type registries

--- Phezu/include/phezu/scene/components/DataComponent.hpp
+++ Phezu/include/phezu/scene/components/DataComponent.hpp
@@ -5,13 +5,9 @@
 #include <typeindex>
 #include <functional>
 
-#define REGISTER_COMPONENT(X) \
-    bool Is_##X##_Registered = Phezu::RegisterComponent(#X, typeid(X), [](std::shared_ptr<Phezu::Entity> entity) { entity->AddComponent<X>(); });
-
 namespace Phezu {
     
     class Entity;
-    bool RegisterComponent(const std::string& typeName, std::type_index typeIndex, std::function<void(std::shared_ptr<Entity>)> constructor);
     
     class DataComponent {
     public:
@@ -21,16 +17,5 @@ namespace Phezu {
         Entity* GetEntity() const { return m_Entity; }
     protected:
         Entity* const m_Entity;
-        
-        
-        /*------- Type Registries ------*/
-    public:
-        static const std::string& GetTypeName(std::type_index typeIndex);
-        static std::function<void(std::shared_ptr<Entity>)> GetComponentConstructor(std::string componentName);
-    private:
-        static std::unordered_map<std::type_index, std::string>& GetTypeRegistry();
-        static std::unordered_map<std::string, std::function<void(std::shared_ptr<Entity>)>>& GetConstructorRegistry();
-        
-        friend bool RegisterComponent(const std::string& typeName, std::type_index typeIndex, std::function<void(std::shared_ptr<Entity>)> constructor);
     };
 }
