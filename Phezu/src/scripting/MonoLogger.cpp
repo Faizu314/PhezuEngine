452522fa0fd41b7078c3a0ba4cdff77983ad55cb
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2025-07-13 18:50:23 +0500

    Dotnet is not a dependency anymore
    Using C# compiler of mono to build dll instead of dotnet
    Dll name is no longer dependent on the cmake target
    Not using mono logger on mac platform (Not required)
    Cleanup in Runtime cmake

--- Phezu/src/scripting/MonoLogger.cpp
+++ Phezu/src/scripting/MonoLogger.cpp
@@ -1,14 +1,12 @@
+#if defined(_WIN32)
+
 #include "scripting/MonoLogger.hpp"
 #include "Logger.hpp"
-
 #include <cstdio>
 #include <cstring>
 #include <iostream>
-
-#if defined(_WIN32)
 #include <io.h>
 #include <fcntl.h>
-#endif
 
 namespace Phezu {
 
@@ -17,7 +15,6 @@ namespace Phezu {
     void MonoLogger::Start() {
         m_Running = true;
 
-#if defined(_WIN32)
         // Create pipe
         SECURITY_ATTRIBUTES saAttr = {};
         saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
@@ -36,33 +33,15 @@ namespace Phezu {
         setvbuf(stdout, NULL, _IONBF, 0);
 
         m_PipeRead = readHandle;
-#elif __APPLE__
-        int pipefd[2];
-        pipe(pipefd);
-
-        // Redirect stdout
-        dup2(pipefd[1], STDOUT_FILENO);
-        dup2(pipefd[1], STDERR_FILENO);
-
-        m_PipeRead = pipefd[0];
-#endif
 
         m_ReadThread = std::thread([this]() {
             char buffer[BUFFER_SIZE];
             while (m_Running) {
-#if defined(_WIN32)
                 DWORD bytesRead;
                 if (ReadFile(m_PipeRead, buffer, sizeof(buffer) - 1, &bytesRead, NULL) && bytesRead > 0) {
                     buffer[bytesRead] = '\0';
                     OnLog(buffer);
                 }
-#elif __APPLE__
-                ssize_t bytesRead = read(m_PipeRead, buffer, sizeof(buffer) - 1);
-                if (bytesRead > 0) {
-                    buffer[bytesRead] = '\0';
-                    OnLog(buffer);
-                }
-#endif
             }
         });
     }
@@ -76,3 +55,5 @@ namespace Phezu {
         Log("Mono: %s\n", msg);
     }
 }
+
+#endif
