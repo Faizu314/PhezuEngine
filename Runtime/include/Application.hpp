b8091ce62126dd2e310c8ed258284534948ddc01
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2025-07-20 18:36:49 +0500

    Using paths from RuntimeDefines
    ScriptEngine is getting paths from engine instead of hardcoding

--- Runtime/include/Application.hpp
+++ Runtime/include/Application.hpp
@@ -5,6 +5,7 @@
 #endif
 
 #include <filesystem>
+#include "RuntimeDefines.hpp"
 
 namespace PhezuRuntime {
     
@@ -17,17 +18,33 @@ namespace PhezuRuntime {
     std::filesystem::path GetExePath(const char* argv[]) {
         return std::filesystem::path(argv[0]).parent_path();
     }
+    
+    std::filesystem::path GetScriptCoreDllPath(const char* argv[]) {
+        return GetExePath(argv) / SCRIPT_CORE_DLL_RELATIVE_PATH;
+    }
+    
+    std::filesystem::path GetMonoCoreLibsPath(const char* argv[]) {
+        return GetExePath(argv) / MONO_CORE_LIBS_RELATIVE_PATH;
+    }
         
 #elif _WIN32
 
-    std::string GetExePath() {
+    std::filesystem::path GetExePath() {
         wchar_t path[MAX_PATH];
         GetModuleFileNameW(NULL, path, MAX_PATH);
-        return std::filesystem::path(path).parent_path().string();
+        return std::filesystem::path(path).parent_path();
     }
 
-    std::string GetProjectPath() {
+    std::filesystem::path GetProjectPath() {
         return GetExePath();
+    }
+    
+    std::filesystem::path GetScriptCoreDllPath() {
+        return GetExePath() / SCRIPT_CORE_DLL_RELATIVE_PATH;
+    }
+    
+    std::filesystem::path GetMonoCoreLibsPath() {
+        return GetExePath() / MONO_CORE_LIBS_RELATIVE_PATH;
     }
     
 #endif
