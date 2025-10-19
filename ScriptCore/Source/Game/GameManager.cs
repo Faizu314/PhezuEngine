6f734e0db1d44e095c9a61452ff423e8b4d53a39
Faizan Qaiser <faizan.qaiser@mindstormstudios.com>
2025-10-19 17:46:09 +0500

    Added level loading logic

--- ScriptCore/Source/Game/GameManager.cs
+++ ScriptCore/Source/Game/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using PhezuEngine;
 using System.Collections.Generic;
 
@@ -6,27 +5,76 @@ namespace Game
 {
     public class GameManager : BehaviourComponent
     {
+        private LevelData[] Levels;
         private PrefabRef m_BrickPrefabRef;
-
         private List<Entity> m_Bricks;
 
         private void OnCreate()
         {
+           Levels = new LevelData[1] {
+                new LevelData()
+                {
+                    GridData = new int[,]
+                    {
+                        { 0, 0, 0, 1 },
+                        { 1, 1, 0, 0 }
+                    },
+                    CellSize = new Vector2(3.1f, 1.1f),
+                    TopLeft = new Vector2(-8f, 9f)
+                }
+            };
+            
             m_Bricks = new();
-            
-            Entity brick = Entity.Instantiate(m_BrickPrefabRef);
-            Console.WriteLine("HasBall: " + brick.HasComponent<Ball>());
-            brick.RemoveComponent<Ball>();
 
-            
-            var transform = brick.GetComponent<Transform>();
-            transform.Position = new Vector2(0f, 100f);
+            LoadLevel(0);
         }
 
-        private void LoadLevel(int[,] levelData)
+        private void LoadLevel(int levelIndex)
         {
-            Entity brick = Entity.Instantiate(m_BrickPrefabRef);
-            m_Bricks.Add(brick);
+            Vector2 topLeft = Levels[levelIndex].TopLeft;
+            Vector2 cellSize = Levels[levelIndex].CellSize;
+            int[,] grid = Levels[levelIndex].GridData;
+
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                for (int y = 0; y < grid.GetLength(0); y++)
+                {
+                    Vector2 worldPos = GridToWorldPosition(x, y, topLeft, cellSize);
+                    
+                    switch (grid[y, x])
+                    {
+                        case 0:
+                            continue;
+                        case 1:
+                            Entity brick = Entity.Instantiate(m_BrickPrefabRef);
+                            brick.GetComponent<Transform>().Position = worldPos;
+                            m_Bricks.Add(brick);
+                            break;
+                    }
+                }
+            }
         }
+
+        private void UnloadLevel()
+        {
+            foreach (var brick in m_Bricks)
+                brick.Destroy();
+            m_Bricks.Clear();
+        }
+
+        private Vector2 GridToWorldPosition(int gridX, int gridY, Vector2 topLeft, Vector2 cellSize)
+        {
+            float x = topLeft.X + gridX * cellSize.X;
+            float y = topLeft.Y - gridY * cellSize.Y;
+            
+            return new Vector2(x, y);
+        }
+    }
+    
+    public struct LevelData
+    {
+        public int[,] GridData;
+        public Vector2 CellSize;
+        public Vector2 TopLeft;
     }
 }
