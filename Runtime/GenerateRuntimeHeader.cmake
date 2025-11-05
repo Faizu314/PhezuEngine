79535529306dca976ce7420fea71e9e5cc251542
Faizan Qaiser <faizanqaiser9@gmail.com>
2025-11-05 21:20:40 +0500

    Removed runtime header generation

--- Runtime/GenerateRuntimeHeader.cmake
+++
@@ -1,3 +0,0 @@
-file(WRITE "${OUT_FILE}" "#pragma once\n\n")
-file(APPEND "${OUT_FILE}" "#define SCRIPT_CORE_DLL_RELATIVE_PATH \"${SCRIPT_PATH}\"\n")
-file(APPEND "${OUT_FILE}" "#define MONO_CORE_LIBS_RELATIVE_PATH \"${MONO_PATH}\"\n")
