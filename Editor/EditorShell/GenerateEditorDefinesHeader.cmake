96082afc7f7dd2320359fc65ad3337e5639f6e2f
Faizan Qaiser <faizanqaiser9@gmail.com>
2025-11-15 16:28:29 +0500

    Fixed header generation

--- Editor/EditorShell/GenerateEditorDefinesHeader.cmake
+++ Editor/EditorShell/GenerateEditorDefinesHeader.cmake
@@ -1,3 +1,2 @@
 file(WRITE "${OUT_FILE}" "#pragma once\n\n")
-file(APPEND "${OUT_FILE}" "#define CSHARP_BUILD_COMMAND \"${CSHARP_BUILD_COMMAND}\"\n")
-file(APPEND "${OUT_FILE}" "#define PROJECT_SRC_DIR \"${PROJECT_SRC_DIR}\"\n")
+file(APPEND "${OUT_FILE}" "#define CSHARP_BUILD_COMMAND ${CSHARP_BUILD_COMMAND}\n")
