684566c7ba1ddf185635b66d4dd7ce11dfb29b6f
Faizan Qaiser <faizanqaiser9@gmail.com>
2025-12-13 16:54:21 +0500

    Added RenderBox functionality in OpenGLAPI
    Rendering without buffers, shader objects, uniforms, textures

--- Phezu/Include/Graphics/OpenGL/OpenGLAPI.hpp
+++ Phezu/Include/Graphics/OpenGL/OpenGLAPI.hpp
@@ -4,11 +4,14 @@
 
 namespace Phezu {
 
+	class IShader;
+
 	class OpenGLAPI : public IGraphicsAPI {
 	public:
 		void SetViewport(int x, int y, int width, int height) override;
 	public:
 		void ClearFrame(Color color) override;
-		void RenderBox(Vector2 downLeftScreen, Vector2 upRightScreen, Color tint) override;
+		void RenderBox(Vector2 downLeftScreenNormalized, Vector2 upRightScreenNormalized, Color tint) override;
+		IShader* CreateShader(const std::string& vert, const std::string& frag) override;
 	};
 }
--- Phezu/Source/Graphics/OpenGL/OpenGLAPI.cpp
+++ Phezu/Source/Graphics/OpenGL/OpenGLAPI.cpp
@@ -3,12 +3,49 @@
 #include "glad/glad.h"
 
 #include "Graphics/OpenGL/OpenGLAPI.hpp"
+#include "Graphics/OpenGL/GLShader.hpp"
 #include "Core/Platform.hpp"
 
 namespace Phezu {
 
-	void OpenGLAPI::RenderBox(Vector2 downLeftScreen, Vector2 upRightScreen, Color tint) {
+	void OpenGLAPI::RenderBox(Vector2 dl, Vector2 ur, Color tint) {
+		float vertices[] = {
+			 dl.X(), dl.Y(), 0.0f,
+			 dl.X(), ur.Y(), 0.0f,
+			 ur.X(), ur.Y(), 0.0f,
+			 ur.X(), dl.Y(), 0.0f
+		};
+
+		unsigned int indices[] = {
+			0, 2, 1,
+			0, 3, 2
+		};
+
+		unsigned int vao;
+
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		unsigned int vbo;
 		
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		unsigned int ibo;
+
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glDeleteBuffers(1, &vbo);
+		glDeleteBuffers(1, &ibo);
+		glDeleteVertexArrays(1, &vao);
 	}
 
 	void OpenGLAPI::SetViewport(int x, int y, int width, int height) {
@@ -30,5 +67,10 @@ namespace Phezu {
 		glClear(GL_COLOR_BUFFER_BIT);
 	}
 
+	IShader* OpenGLAPI::CreateShader(const std::string& vert, const std::string& frag) {
+		IShader* shader = new GLShader();
+		shader->Init(vert, frag);
+		return shader;
+	}
 
 }
