f5d0aa8af5c82bdb17214706687ee78f0b969fc4
Faizan Qaiser <faizanqaiser9@gmail.com>
2026-03-17 00:20:07 +0500

    Setting up CMake and Mono test environment

--- Scripts/win32/Helpers/SetupMono.bat
+++ Scripts/win32/Helpers/SetupMono.bat
@@ -5,9 +5,12 @@ set OUTPUT_DIR=%CD%\Vendor\win32\temp\mono
 if not exist "Vendor\win32\mono\.installed" (
     call :ValidateContinuation "Enter Y to continue with Mono installation: "
 
-    mkdir %OUTPUT_DIR%
-    call Scripts\win32\Downloader.bat %PACKAGE% %MONO_LINK% %OUTPUT_DIR%\mono-6.12.0-x64-0.msi
-    call Scripts\win32\MonoInstaller.bat %CD% %OUTPUT_DIR%\mono-6.12.0-x64-0.msi "%OUTPUT_DIR%\extract"
+    if not exist %OUTPUT_DIR% (
+        mkdir %OUTPUT_DIR%
+    )
+    
+    call Scripts\win32\Helpers\Downloader.bat %PACKAGE% %MONO_LINK% %OUTPUT_DIR%\mono-6.12.0-x64-0.msi
+    call Scripts\win32\Helpers\MonoInstaller.bat %CD% %OUTPUT_DIR%\mono-6.12.0-x64-0.msi "%OUTPUT_DIR%\extract"
 
     if %ERRORLEVEL% neq 0 (
         echo Error installing Mono.
