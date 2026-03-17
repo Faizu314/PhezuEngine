2ed9ce03263200bc6039e39365809868f4f9a717
Faizan Qaiser <faizanqaiser9@gmail.com>
2026-03-17 21:51:12 +0500

    Workflow patch

--- Scripts/win32/Tests/CmakeEnvironmentSetup.bat
+++ Scripts/win32/Tests/CmakeEnvironmentSetup.bat
@@ -2,7 +2,7 @@ set CMAKE_ENV=%1
 
 echo "Setting up CMake Environment: %CMAKE_ENV%"
 
-if "%CMAKE_ENV%"=="system" (
+if "%CMAKE_ENV%"=="--system" (
 
 	echo CMake already installed...
 	exit /b 0
@@ -10,9 +10,12 @@ if "%CMAKE_ENV%"=="system" (
 )
 
 echo Hiding CMake installation path...
+where cmake
 
 set "PATH=%PATH:C:\Program Files\CMake\bin;=%"
 set "PATH=%PATH:C:\Program Files (x86)\CMake\bin;=%"
+set "PATH=%PATH:C:\Program Files\CMake\bin=%"
+set "PATH=%PATH:C:\Program Files (x86)\CMake\bin=%"
 
 where cmake >nul 2>&1
 if ERRORLEVEL 1 (
