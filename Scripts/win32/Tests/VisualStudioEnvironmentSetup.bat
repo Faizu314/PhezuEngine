de690142cb2a2d940ab94e70d384260f9e72ac93
Faizan Qaiser <faizanqaiser9@gmail.com>
2026-03-17 20:22:48 +0500

    Action fixes

--- Scripts/win32/Tests/VisualStudioEnvironmentSetup.bat
+++ Scripts/win32/Tests/VisualStudioEnvironmentSetup.bat
@@ -6,38 +6,38 @@ echo Removing Enterprise and Professional Visual Studio Installations...
 call VsRemove.bat "Microsoft.VisualStudio.Product.Enterprise"
 call VsRemove.bat "Microsoft.VisualStudio.Product.Professional"
 
-if "%VS_ENV%"=="ide_full" (
+if "%VS_ENV%"=="--ide_full" (
 
 	echo Uninstalling Visual Studio Build Tools...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.BuildTools"
 
-) else if "%VS_ENV%"=="ide_partial" (
+) else if "%VS_ENV%"=="--ide_partial" (
 
 	echo Uninstalling Visual Studio Build Tools...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.BuildTools"
 	echo Uninstalling component: Microsoft.VisualStudio.Component.VC.Tools.x86.x64 of Visual Studio IDE...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.Community" "Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 
-) else if "%VS_ENV%"=="ide_missing_workload" (
+) else if "%VS_ENV%"=="--ide_missing_workload" (
 
 	echo Uninstalling Visual Studio Build Tools...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.BuildTools"
 	echo Uninstalling workload: Microsoft.VisualStudio.Workload.NativeDesktop of Visual Studio IDE...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.Community" "Microsoft.VisualStudio.Workload.NativeDesktop"
 
-) else if "%VS_ENV%"=="bt_full" (
+) else if "%VS_ENV%"=="--bt_full" (
 
 	echo Uninstalling Visual Studio Community IDE...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.Community"
 
-) else if "%VS_ENV%"=="bt_partial" (
+) else if "%VS_ENV%"=="--bt_partial" (
 
 	echo Uninstalling Visual Studio Community IDE...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.Community"
 	echo Uninstalling component: Microsoft.VisualStudio.Component.VC.Tools.x86.x64 of Visual Studio Build Tools...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.BuildTools" "Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 
-) else if "%VS_ENV%"=="bt_missing_workload" (
+) else if "%VS_ENV%"=="--bt_missing_workload" (
 
 	echo Uninstalling Visual Studio Community IDE...
 	call VsRemove.bat "Microsoft.VisualStudio.Product.Community"
--- Scripts/win32/Tests/VsRemove.bat
+++ Scripts/win32/Tests/VsRemove.bat
@@ -10,10 +10,10 @@ if "%PRODUCT_ID%"=="" (
     exit /b 1
 )
 
-set VSWHERE="C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-set INSTALLER="C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe"
+set VSWHERE=C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe
+set INSTALLER=C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe
 
-if not exist %VSWHERE% (
+if not exist %"VSWHERE"% (
     echo vswhere not found
     exit /b 1
 )
@@ -21,13 +21,13 @@ if not exist %VSWHERE% (
 echo Searching for product: %PRODUCT_ID%
 
 if "%COMPONENT_ID%"=="" (
-    for /f "usebackq tokens=*" %%i in (`%VSWHERE% -products %PRODUCT_ID% -property installationPath`) do (
+    for /f "usebackq tokens=*" %%i in (`%"VSWHERE"% -products %PRODUCT_ID% -property installationPath`) do (
 
         set INSTALL_PATH=%%i
         echo Found installation: !INSTALL_PATH!
 
         echo Removing product
-        %INSTALLER% uninstall ^
+        %"INSTALLER"% uninstall ^
             --installPath "!INSTALL_PATH!" ^
             --passive --norestart
     )
@@ -41,13 +41,13 @@ if "%COMPONENT_ID%"=="" (
     exit /b 0
 )
 
-for /f "usebackq tokens=*" %%i in (`%VSWHERE% -products %PRODUCT_ID% -requires %COMPONENT_ID% -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`%"VSWHERE"% -products %PRODUCT_ID% -requires %COMPONENT_ID% -property installationPath`) do (
 
     set INSTALL_PATH=%%i
     echo Found installation: !INSTALL_PATH!
 
     echo Removing component %COMPONENT_ID%
-    %INSTALLER% modify ^
+    %"INSTALLER"% modify ^
         --installPath "!INSTALL_PATH!" ^
         --remove %COMPONENT_ID% ^
         --passive --norestart
