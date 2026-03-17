8f73dfc1e0a421273fef0646803464d89e889ed0
Faizan Qaiser <faizanqaiser9@gmail.com>
2026-03-17 22:25:08 +0500

    Fixes
    Leftover mono files

--- Scripts/win32/Tests/MonoEnvironmentSetup.bat
+++ Scripts/win32/Tests/MonoEnvironmentSetup.bat
@@ -2,7 +2,7 @@ set MONO_ENV=%1
 
 echo "Setting up CMake Environment: %MONO_ENV%"
 
-if "%MONO_ENV%"=="mono" (
+if "%MONO_ENV%"=="--mono" (
 
 	echo Installing mono
 	call Scripts\win32\Helpers\SetupMono.bat
